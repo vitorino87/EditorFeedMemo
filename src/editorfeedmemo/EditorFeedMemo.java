7dcce89fdf9da943d2acab9147308532cb8f0313
vitorino87 <brother_87_22@hotmail.com>
2019-07-30 19:42:57 -0300

    adicionado botão save as..., save e suas funcionalidades

--- src/editorfeedmemo/EditorFeedMemo.java
+++ src/editorfeedmemo/EditorFeedMemo.java
@@ -13,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
@@ -57,8 +58,11 @@ public class EditorFeedMemo extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
 
         javax.swing.GroupLayout jDialog1Layout = new javax.swing.GroupLayout(jDialog1.getContentPane());
         jDialog1.getContentPane().setLayout(jDialog1Layout);
@@ -108,7 +112,7 @@ public class EditorFeedMemo extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 262, Short.MAX_VALUE)
                 .addGap(32, 32, 32))
         );
 
@@ -128,31 +132,42 @@ public class EditorFeedMemo extends javax.swing.JFrame {
 
         jLabel2.setText("FeedMemo2 Keywords:");
 
-        jButton2.setText("Adicionar tag");
+        jButton2.setText("Add tag");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
 
-        jButton1.setText("Salvar");
+        jButton1.setText("Save as...");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        jLabel4.setText("Colunas: ");
+        jLabel4.setText("Colunas: 0");
 
-        jLabel5.setText("0");
-
-        jButton3.setText("Limpar");
+        jButton3.setText("Clear text");
         jButton3.setMaximumSize(new java.awt.Dimension(95, 23));
         jButton3.setMinimumSize(new java.awt.Dimension(95, 23));
         jButton3.setPreferredSize(new java.awt.Dimension(95, 23));
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Tag: 0");
+
+        jLabel6.setText("Local para salvar FeedMemo3:");
+
+        jLabel5.setText("Local para salvar FeedMemo2:");
+
+        jButton4.setText("Save");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
             }
         });
 
@@ -163,25 +178,26 @@ public class EditorFeedMemo extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 961, Short.MAX_VALUE)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 961, Short.MAX_VALUE)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButton2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(25, 25, 25))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel5)
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel5))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -189,17 +205,25 @@ public class EditorFeedMemo extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 280, Short.MAX_VALUE)
-                .addGap(3, 3, 3)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel5))
-                .addGap(1, 1, 1)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(3, 3, 3)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel5)))
+                .addGap(23, 23, 23))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -232,10 +256,12 @@ public class EditorFeedMemo extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         JFileChooser jf = new JFileChooser();
         JTextArea[] j = {jTextArea4, jTextArea2};
+        JLabel[] l = {jLabel6, jLabel5};
         for (int i = 0; i < 2; i++) {
             quebrar_Salvar_String(j[i], i);
             if (jf.showSaveDialog(jPanel1) == JFileChooser.APPROVE_OPTION) {
-                File f = jf.getSelectedFile();
+                File f = jf.getSelectedFile();                
+                l[i].setText(l[i].getText()+" "+f.getAbsolutePath());
                 if (f != null) {
                     try (Writer fw2 = new OutputStreamWriter(new FileOutputStream(f, true), StandardCharsets.UTF_8)) {
                         Iterator<String> it = ar[i].iterator();
@@ -292,33 +318,33 @@ public class EditorFeedMemo extends javax.swing.JFrame {
     public void quebrar_Salvar_String(javax.swing.JTextArea j, int i) {
         String text3 = j.getText();          //string bruta
         String bkp = "";
-        if (!text3.equals("")) {
-            while (text3.indexOf('\n') > 0 || text3.length() > 73) {
-                int a = text3.indexOf('\n');        //1� trecho encontrado \n
-                if (a != -1) {
-                    String aux = text3.substring(0, a);  //separando primeiro trecho
-                    text3 = text3.substring(a + 1, text3.length());
-                    bkp += quebrar_Salvar_String(aux, i);
-                } else {       //se n�o encontrou nenhum \n ent�o apenas quebre e save
-                    bkp += quebrar_Salvar_String(text3, i);
-                    break;
-                }
+        while (text3.indexOf('\n') > 0 || text3.length() > 73) {
+            int a = text3.indexOf('\n');        //1� trecho encontrado \n
+            if (a != -1) {
+                String aux = text3.substring(0, a);  //separando primeiro trecho
+                text3 = text3.substring(a + 1, text3.length());
+                bkp += quebrar_Salvar_String(aux, i);
+            } else {       //se n�o encontrou nenhum \n ent�o apenas quebre e save
+                bkp += quebrar_Salvar_String(text3, i);
+                break;
             }
-            if (text3.length() < 73) {
-                if (i == 0) {
-                    ar[i].add("\"" + text3 + "\"," + tag);
-                } else {
-                    ar[i].add("\"" + text3 + "\",n," + tag);
-                }
-                bkp += text3 + '\n';
-            }
-            //j.setText(bkp);
         }
+        if (text3.length() < 73 && !text3.equals("")) {
+            if (i == 0) {
+                ar[i].add("\"" + text3 + "\"," + tag);
+            } else {
+                ar[i].add("\"" + text3 + "\",n," + tag);
+            }
+            bkp += text3 + '\n';
+        }
+        //j.setText(bkp);
+
     }
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         String aux = JOptionPane.showInputDialog(rootPane, "Informe a tag desejada:", "Adicionando tag", JOptionPane.QUESTION_MESSAGE);
         tag = Integer.valueOf(aux);
+        jLabel3.setText("Tag: " + tag);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jTextArea4KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextArea4KeyReleased
@@ -333,7 +359,7 @@ public class EditorFeedMemo extends javax.swing.JFrame {
             text = text.substring(a + 1, text.length());
         }
         int tam = text.length();
-        jLabel5.setText(String.valueOf(tam));
+        jLabel4.setText("Colunas: " + String.valueOf(tam));
     }//GEN-LAST:event_jTextArea2KeyReleased
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
@@ -341,6 +367,37 @@ public class EditorFeedMemo extends javax.swing.JFrame {
         jTextArea2.setText("");
         jTextArea4.setText("");
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        String t1 = jLabel6.getText();
+        String t2 = jLabel5.getText();
+        t1 = t1.substring(t1.indexOf(":")+1);
+        t2 = t2.substring(t1.indexOf(":")+1);
+        
+        String[] url = {jLabel6.getText().substring(jLabel6.getText().indexOf(":")+1),
+        jLabel5.getText().substring(jLabel5.getText().indexOf(":")+1)};
+        JTextArea[] j = {jTextArea4, jTextArea2};
+        if(!url[0].isEmpty() || !url[1].isEmpty()){
+            for (int i = 0; i < 2; i++) {
+            quebrar_Salvar_String(j[i], i);
+            File f = new File(url[i].trim());
+                if (f != null) {
+                    try (Writer fw2 = new OutputStreamWriter(new FileOutputStream(f, true), StandardCharsets.UTF_8)) {
+                        Iterator<String> it = ar[i].iterator();
+                        while (it.hasNext()) {
+                            fw2.append(it.next() + '\n');
+                        }
+                    } catch (Exception e) {
+                        //System.out.println(e.toString());
+                    }
+                }            
+            ar[i].clear();
+            }
+        }else{
+            jButton1ActionPerformed(evt);
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -385,11 +442,14 @@ public class EditorFeedMemo extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JDialog jDialog1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane2;
